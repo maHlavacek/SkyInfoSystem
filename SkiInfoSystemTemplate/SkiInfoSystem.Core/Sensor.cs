eedd3f1b272f704d5066fa9b7562723683f4c755
maHlavacek <martin_hlavacek@icloud.com>
2019-01-29 15:56:09 +0100

    EventHandler between Slopes and Sensors

--- SkiInfoSystemTemplate/SkiInfoSystem.Core/Sensor.cs
+++ SkiInfoSystemTemplate/SkiInfoSystem.Core/Sensor.cs
@@ -17,11 +17,35 @@ namespace SkiInfoSystem.Core
         public int Id { get; }
         public int SlopeId { get; }
         public MeasurementType MeasurementType { get; }
+        public IEnumerable<Measurement> Measurements { get;private set; }
 
-        
+        private double _lastValeu;
 
         public Sensor(int id, int slopeId, MeasurementType measurementType)
         {
+            Id = id;
+            SlopeId = slopeId;
+            MeasurementType = measurementType;
+            CsvDataProvider measurements = new CsvDataProvider();
+            Measurements = measurements.GetMeasurmentsForSensor(id);
+            FastClock.Instance.OneMinuteIsOver += Instance_OneMinuteIsOver;
+        }
+
+        private void Instance_OneMinuteIsOver(object sender, DateTime time)
+        {
+            foreach (Measurement measurement in Measurements)
+            {
+                if(measurement.Timestamp == time && _lastValeu != measurement.Value)
+                {
+                    _lastValeu = measurement.Value;
+                    OnMeasurementOccured(measurement.Value);                   
+                }
+            }
+        }
+
+        public void OnMeasurementOccured(double value)
+        {
+            MeasurementOccured?.Invoke(this, value);
         }
     }
 }
--- SkiInfoSystemTemplate/SkiInfoSystem.Core/Slope.cs
+++ SkiInfoSystemTemplate/SkiInfoSystem.Core/Slope.cs
@@ -12,9 +12,28 @@ namespace SkiInfoSystem.Core
         public int Id { get; }
         public string Name { get; }
 
+        private IEnumerable<Sensor> _sensors;
+
+
+
         public Slope(int id, string name)
-        {
+        { 
+            Id = id;
+            Name = name;
+            CsvDataProvider sensors = new CsvDataProvider();
+            _sensors = sensors.GetSensorsForSlope(id);
+
+            foreach (Sensor sensor in _sensors)
+            {
+                sensor.MeasurementOccured += Sensor_MeasurementOccured1;
+            }
+
         }
-        
+
+        private void Sensor_MeasurementOccured1(object sender, double e)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
